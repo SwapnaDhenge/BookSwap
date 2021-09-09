1f8f9d22d28334fbd982d071fae08d5ef762b14b
gaurav.dhenge <gauravmd@celstream.com>
2021-09-09 14:41:59 +0530

    Add project files.

---
+++ BookStor.Data/Interfaces/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookSwap.Models;
+
+namespace BookStor.Data.Interfaces
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllBooks();
+        Book GetBook(int id);
+    }
+}
