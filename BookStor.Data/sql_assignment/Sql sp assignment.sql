64eb680de950715cac48f008c2ace17bdcdacda7
gaurav.dhenge <gauravmd@celstream.com>
2021-09-14 17:51:36 +0530

    sql sp assignment

--- BookStor.Data/sql_assignment/Sql sp assignment.sql
+++ BookStor.Data/sql_assignment/Sql sp assignment.sql
@@ -16,7 +16,7 @@ drop table Location2;
 select * from Emp;
 select * from Location3;
 
-create proc spEmp1
+create proc spEm1
 (
 @name varchar(50),
 @location varchar(50),
@@ -35,9 +35,40 @@ declare @id int
 select @id= SCOPE_IDENTITY()
 insert into Location3(location_name,office_size,location_id)values(@location_name,@office_size,@id)
 end 
-exec spEmp1 'swapnali','elec',1,25,'mumbai',30 
-exec spEmp1 'swapna','elec1',3,35,'mumbai1',20 
-exec spEmp1 'Dnyanaa','elec1',3,35,'Pune',20 
+exec spEm1 'swapnali','elec',1,25,'mumbai',30 
+exec spEm1 'swapna','elec1',3,35,'mumbai1',20 
+exec spEm1 'Dnyanaa','elec1',3,35,'Pune',20 
+exec spEm1 
 
 select *from Emp,Location3 where Emp.emp_id=Location3.id;
 select *from Emp where emp_id=5;
+
+
+alter procedure Spfetchdata1 
+(
+@id int
+)
+as
+begin
+ if(isnull(@id,'') !='')
+ begin
+ select E.emp_id,E.name,E.location,E.experience,E.age,E1.location_name,E1.office_size,E1.location_id
+from Emp E join Location3 E1
+on E.emp_id=E1.location_id
+and E.emp_id=@id
+end
+else 
+begin
+select E.emp_id,E.name,E.location,E.experience,E.age,E1.location_name,E1.office_size,E1.location_id
+from Emp E join Location3 E1
+on E.emp_id=E1.location_id
+end
+end 
+exec Spfetchdata1 null
+
+
+
+
+
+
+
